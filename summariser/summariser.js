52e0f2f8e52084c1b9b8bf4e85d39321349fd9b5
Adam Hammond <adam_hammond@uk.ibm.com>
2017-07-21 17:50:57 +0200

    now passes through all of msg to all outputs except msg.payload

--- summariser/summariser.js
+++ summariser/summariser.js
@@ -6,24 +6,25 @@ module.exports = function(RED) {
         var node = this;
 
         node.on('input', function(msg) {
-            // create empty return array
-            var retPayload = _.times(config.rules.length, function() { return {payload: ""}});
             var data = msg.payload;
-
+            
+            // create return array
+            msg.payload = "";
+            var retPayload = _.times(config.rules.length, function() { return msg });
+            
             // if msg.payload is an array, process the rules
             if (_.isArray(data)) {
               config.rules.forEach(function(rule, idx) {
-                retPayload[idx] = {payload: rule.field}
                 if (rule.op == "sum") {
-                  retPayload[idx] = {payload: _.sumBy(data, rule.field)}
+                  retPayload[idx]["payload"] = _.sumBy(data, rule.field)
                 } else if (rule.op == "count") {
-                  retPayload[idx] = {payload: _.map(data, rule.field).length}
+                  retPayload[idx]["payload"] = _.map(data, rule.field).length
                 } else if (rule.op == "mean") {
-                  retPayload[idx] = {payload: _.meanBy(data, rule.field)}
+                  retPayload[idx]["payload"] = _.meanBy(data, rule.field)
                 } else if (rule.op == "group") {
-                  retPayload[idx] = {payload: _.countBy(data, rule.field)}
+                  retPayload[idx]["payload"] = _.countBy(data, rule.field)
                 } else if (rule.op == "join") {
-                  retPayload[idx] = {payload: _.map(data, rule.field).join(rule.sep)}
+                  retPayload[idx]["payload"] = _.map(data, rule.field).join(rule.sep)
                 }
               })
             };
